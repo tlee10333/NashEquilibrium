fc7e2070c941679a46e73688707fa51a55709f26
Daniel Quinteros <47285707+d-quinteros@users.noreply.github.com>
2024-02-22 21:19:37 -0500

    Added payoff history and manual strategy declaration
    Made attribute and corresponding functions for a payoff history. Included a method for setting the Prisoner strategy manually.

--- code/Prisoner.py
+++ code/Prisoner.py
@@ -1,14 +1,22 @@
 import random
 
 class Prisoner:
-    def __init__(self):
-        strategies = ['C', 'D']
-        self.strategy = random.choice(strategies)
+    def __init__(self, strat=None):
+        possible_strats = ['C', 'D']
+        self.strategy = strat
+        
+        if strat is None:
+            self.strategy = random.choice(possible_strats)
+        
         self.TRSP = {'T': 1, 'R': 2, 'P': 3, 'S': 4}
         self.strategy_history = [self.strategy]
+        self.payoff_history = []
 
     def get_strategy(self):
         return self.strategy
+    
+    def set_strategy(self, x):
+        self.strategy = x
     
     def get_payoff(self, j):
         if self.strategy == 'D' and j.get_strategy() == 'C':
@@ -22,14 +30,23 @@ class Prisoner:
 
     def update_payoff(self, j):
         if self.strategy == 'D' and j.get_strategy() == 'C':
+            self.payoff_history.append(self.TRSP['T'])
             self.TRSP['T'] += 1
         elif self.strategy == 'D' and j.get_strategy() == 'D':
+            self.payoff_history.append(self.TRSP['P'])
             self.TRSP['P'] += 1
         elif self.strategy == 'C' and j.get_strategy() == 'C':
+            self.payoff_history.append(self.TRSP['R'])
             self.TRSP['R'] += 1
         else:
+            self.payoff_history.append(self.TRSP['S'])
             self.TRSP['S'] += 1
 
-    def add_to_history(self, x):
+    def get_previous_payoff(self):
+        return self.payoff_history[-1]
+
+    def add_to_strat_hist(self, x):
         self.strategy_history.append(x)
-        
+
+    def strat_history_append(self, x):
+        self.strategy_history.append(x)
