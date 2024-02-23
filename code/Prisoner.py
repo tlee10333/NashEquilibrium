6a7d0ad6b292827ac9c4f61eb8459f4a3546fb72
Daniel Quinteros <47285707+d-quinteros@users.noreply.github.com>
2024-02-22 22:18:17 -0500

    Restructured payoff
    Changed the payoff to be added on to per 2-player-game. Code infrastructure cleaned

--- code/Prisoner.py
+++ code/Prisoner.py
@@ -2,51 +2,67 @@ import random
 
 class Prisoner:
     def __init__(self, strat=None):
+        """
+        Implementation of the Prisoner object in the Prisoner's Dilemma Game
+
+        Attributes:
+            strat: a string (C or D) representing the Prisoner's initial strategy
+        """
         possible_strats = ['C', 'D']
         self.strategy = strat
         
         if strat is None:
             self.strategy = random.choice(possible_strats)
         
-        self.TRSP = {'T': 1, 'R': 2, 'P': 3, 'S': 4}
+        self.TRPS = {'T': 1, 'R': 2, 'P': 3, 'S': 4}
         self.strategy_history = [self.strategy]
+        self.payoff = 0
         self.payoff_history = []
 
-    def get_strategy(self):
-        return self.strategy
-    
+    ### STRATEGY ###
     def set_strategy(self, x):
         self.strategy = x
+
+    def get_strategy(self):
+        """
+        Gets the Prisoner's current strategy
+
+        Returns:
+            A string (C or D) representing the Prisoner's current strategy
+        """
+        return self.strategy
     
-    def get_payoff(self, j):
-        if self.strategy == 'D' and j.get_strategy() == 'C':
-            return self.TRSP['T']
-        elif self.strategy == 'D' and j.get_strategy() == 'D':
-            return self.TRSP['P']
-        elif self.strategy == 'C' and j.get_strategy() == 'C':
-            return self.TRSP['R']
-        else:
-            return self.TRSP['S']
-
-    def update_payoff(self, j):
-        if self.strategy == 'D' and j.get_strategy() == 'C':
-            self.payoff_history.append(self.TRSP['T'])
-            self.TRSP['T'] += 1
-        elif self.strategy == 'D' and j.get_strategy() == 'D':
-            self.payoff_history.append(self.TRSP['P'])
-            self.TRSP['P'] += 1
-        elif self.strategy == 'C' and j.get_strategy() == 'C':
-            self.payoff_history.append(self.TRSP['R'])
-            self.TRSP['R'] += 1
-        else:
-            self.payoff_history.append(self.TRSP['S'])
-            self.TRSP['S'] += 1
-
+    def add_to_strategy_history(self, x):
+        self.strategy_history.append(x)
+    
+    ### PAYOFF ###
+    def get_payoff(self):
+        return self.payoff
+    
     def get_previous_payoff(self):
         return self.payoff_history[-1]
 
-    def add_to_strat_hist(self, x):
-        self.strategy_history.append(x)
+    def update_payoff(self, j):
+        if self.strategy == 'D' and j.get_strategy() == 'C':
+            self.payoff += self.TRPS['T']
+        elif self.strategy == 'D' and j.get_strategy() == 'D':
+            self.payoff += self.TRPS['P']
+        elif self.strategy == 'C' and j.get_strategy() == 'C':
+            self.payoff += self.TRPS['R']
+        else:
+            self.payoff += self.TRPS['S']
 
-    def strat_history_append(self, x):
-        self.strategy_history.append(x)
+    def reset_payoff(self):
+        self.payoff = 0
+    
+    def add_to_payoff_history(self):
+        self.payoff_history.append(self.payoff)
+
+    ### OTHER ###
+
+    def local_frequency(self):
+        target = 'C'
+        num_coop_rounds = [i for i in self.strategy_history if i==target]
+        num_rounds_total = len(self.strategy_history)
+        coop_rate = num_coop_rounds / num_rounds_total
+        return coop_rate
